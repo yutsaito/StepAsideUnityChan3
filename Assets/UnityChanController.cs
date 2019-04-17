5ad850c07fc3579ac2755aa498ad3534cf61752a
yutakasaito <fvkibdcz7@yahoo.co.jp>
2019-04-17 19:57:57 +0900

    Lesson6  StepAsideUnityChan
    メンターさん
    4.4まで進めて再生してみたところ、Game画面にUnitychanがいません。
    SceneのCameraViewには映ってます。
    どこが悪いのでしょうか？
    ご指導お願いします。

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{   //ｱﾆﾒｰｼｮﾝするためのｺﾝﾎﾟｰﾈﾝﾄを入れる
+    private Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Animatorｺﾝﾎﾟｰﾈﾝﾄを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るｱﾆﾒｰｼｮﾝを開始
+        this.myAnimator.SetFloat("Speed", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
